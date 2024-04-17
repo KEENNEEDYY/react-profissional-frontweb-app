b763f2aa8ee1ec01d74659c554c4435200ac6b22
KEENNEEDYY <kennedy.costa.valenet@gmail.com>
2024-04-17 11:57:17 -0300

    Searchbar onSearch event
    Signed-off-by: KEENNEEDYY <kennedy.costa.valenet@gmail.com>

--- src/components/SeachBar/index.tsx
+++ src/components/SeachBar/index.tsx
@@ -1,11 +1,40 @@
+import { useState } from 'react';
 import './styles.css';
 
-export default function SearchBar() {
+type Props = {
+    onSearch: Function;
+}
+
+export default function SearchBar({onSearch} : Props) {
+
+    const [text, setText] = useState("");
+    
+    function handleSubmit(event: any){
+        event.preventDefault();
+        onSearch(text);
+    }
+
+    function handleChange(event: any){
+        const value = event.target.value;
+        setText(value);
+        onSearch(value);
+    }
+
+    function handleResetClick(){
+        setText("");
+        onSearch(text);
+    }
+    
     return (
-        <form className="dsc-search-bar">
-            <button type="submit">🔎︎</button>
-            <input type="text" placeholder="Nome do produto" />
-            <button type="reset">🗙</button>
+        <form className="dsc-search-bar" onSubmit={handleSubmit}>
+            <button type="submit" onClick={handleSubmit}>🔎︎</button>
+            <input 
+                value={text}
+                type="text" 
+                placeholder="Nome do produto"
+                onChange={handleChange} 
+            />
+            <button onClick={handleResetClick}>🗙</button>
         </form>
     )
 }
